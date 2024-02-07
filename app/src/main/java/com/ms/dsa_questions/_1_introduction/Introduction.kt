6320e5976e519d440ab35dd955c37e8deffafd02
mudit <Vau63bOe1NREBTiim+rioBvOWOU6tYZTaPcfAM/5zAs=>
2024-02-07 20:39:44 +0530

    INTRODUCTION -sumOfNNumbers -giveComplexity

--- app/src/main/java/com/ms/dsa_questions/_1_introduction/Introduction.kt
+++ app/src/main/java/com/ms/dsa_questions/_1_introduction/Introduction.kt
@@ -1,5 +1,25 @@
 package com.ms.dsa_questions._1_introduction
 
-fun main(){
+val TAG = "INTRODUCTION"
 
+fun main() {
+//    println(sumOfNNumbers(10))
+
+    giveComplexity(10)
+}
+
+fun sumOfNNumbers(n: Int): Int {
+    return n.times(n.inc()).div(2)
+}
+
+fun giveComplexity(n: Int) {
+    var count = 0
+    for (i in 0 until n) {
+        for (j in i downTo 0) {
+            count += 1
+            println("giveComplexity: i : $i  |  j : $j   |  count : $count")
+        }
+        println()
+    }
+//    Log.e(TAG, "giveComplexity: count - "+count )
 }
